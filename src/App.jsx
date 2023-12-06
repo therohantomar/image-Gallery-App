d748c555fdaaacba7e9519d5bd8ad85aeabaddf5
therohantomar <therohantomar@gmail.com>
2023-12-06 23:28:26 +0530

    little bit styles changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,14 @@ import ImageGrid from "./components/ImageGrid";
 import { Provider } from "react-redux";
 import { store } from "./utils/store";
 
-
 function App() {
   return (
-    <Provider store={store}>
-      <main className="container">
+    <main className="container">
+      <Provider store={store}>
         <Header />
-        <ImageGrid/>
-      </main>
-    </Provider>
+        <ImageGrid />
+      </Provider>
+    </main>
   );
 }
 
